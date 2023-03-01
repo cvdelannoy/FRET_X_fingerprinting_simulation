b9574e1bf6c4a5d906e57d755dc99bedd13f13fa
Carlos de Lannoy <cvdelannoy@gmail.com>
2023-03-01 11:13:51 +0100

    asyn classifier: more plots and small fixes

--- asyn_classifier/asyn_classifier_pipeline.py
+++ asyn_classifier/asyn_classifier_pipeline.py
@@ -23,15 +23,15 @@ def numeric_timestamp():
     return int(datetime.now().strftime('%H%M%S%f'))
 
 def plot_confusion_heatmap(df, true_var, pred_var, fold_var, var_names, out_dir):
-    confusion_dict = {vn:{vn:[] for vn in var_names} for vn in var_names}
+    confusion_dict = {vn:{vn: 0.0 for vn in var_names} for vn in var_names}
     confusion_dict_sd = deepcopy(confusion_dict)
     for (true_id, pred_id), sdf in df.groupby([true_var, pred_var]):
         fold_sizes = {nb_fold: len(ssdf) for nb_fold, ssdf in df.query(f'{true_var} == "{true_id}"').groupby(fold_var)}
-        frac_pred = [len(ssdf) / fold_sizes[nb_fold] for nb_fold, ssdf in sdf.groupby(fold_var)]
-        confusion_dict[true_id][pred_id] = np.mean(frac_pred)
+        frac_pred = np.array([len(ssdf) / fold_sizes[nb_fold] for nb_fold, ssdf in sdf.groupby(fold_var)])
+        confusion_dict[true_id][pred_id] = np.mean(frac_pred) if np.any(frac_pred > 0) else 0
         confusion_dict_sd[true_id][pred_id] = np.std(frac_pred)
-    heat_df = pd.DataFrame.from_dict(confusion_dict)
-    heat_df_sd = pd.DataFrame.from_dict(confusion_dict_sd)
+    heat_df = pd.DataFrame.from_dict(confusion_dict).T
+    heat_df_sd = pd.DataFrame.from_dict(confusion_dict_sd).T
     annot_mat = (heat_df.round(2).astype(str) + '\n±' + heat_df_sd.round(2).astype(str))
     heat_df.rename_axis('Truth', axis='rows', inplace=True)
     heat_df.rename_axis('Predicted', axis='columns', inplace=True)
@@ -45,6 +45,26 @@ def plot_confusion_heatmap(df, true_var, pred_var, fold_var, var_names, out_dir)
         plt.savefig(f'{out_dir}heatmap_confmat_{cmap}.svg', dpi=400)
         plt.close(fig)
 
+def plot_precision_recall(df, true_var, pred_var, out_dir):
+    recall = {cl_id: len(sdf.query(f'{true_var} == {pred_var}')) / len(sdf) for cl_id, sdf in df.groupby(true_var)}
+    precision = {cl_id: len(sdf.query(f'{true_var} == {pred_var}')) / len(sdf) for cl_id, sdf in df.groupby(pred_var)}
+    pr_df = pd.DataFrame.from_dict({'precision': precision, 'recall': recall})
+    pr_df.to_csv(f'{out_dir}precision_recall.csv')
+    fig, ax = plt.subplots(figsize=(5,5))
+    sns.scatterplot(x='recall', y='precision', data=pr_df, ax=ax)
+    for lab, tup in pr_df.iterrows():
+        ax.text(tup.recall + 0.01, tup.precision, str(lab))
+    ax.set_xlim([0,1])
+    ax.set_ylim([0,1])
+    fig.savefig(f'{out_dir}precision_recall.svg')
+
+
+def get_stats(df, true_var, pred_var, out_dir):
+    overall_acc = np.mean(df.loc[:, true_var] == df.loc[:, pred_var])
+    with open(f'{out_dir}stats.yaml', 'w') as fh:
+        fh.write(f'''
+overall_acc: {overall_acc}
+        ''')
 
 
 def main(data_dict, nb_folds, out_dir, regex, cores):
@@ -77,12 +97,15 @@ def main(data_dict, nb_folds, out_dir, regex, cores):
     snakemake(sm_fn, cores=cores)
     pred_df = pd.read_csv(f'{out_dir}asyn_test_predicted_all.csv', index_col=0)
     var_names = sorted(pred_df.cl_id.unique(), key=lambda x: int(re.search('(?<=[A-Z])[0-9]+(?=[A-Z])', x).group(0)))
+    get_stats(pred_df, 'cl_id', 'pred', out_dir)
     plot_confusion_heatmap(pred_df, 'cl_id', 'pred', 'fold', var_names, out_dir)
+    plot_precision_recall(pred_df, 'cl_id', 'pred', out_dir)
+
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Train & evaluate aSyn classification.')
-    parser.add_argument('--in-dir', type=str, required=True)
+    parser.add_argument('--in-dir', type=str, nargs='+', required=True)
     parser.add_argument('--regex', type=str, default='[A-Z][0-9]+[A-Z](?=.txt)')
     parser.add_argument('--nb-folds',type=int, default=10)
     parser.add_argument('--out-dir', type=str, required=True)
